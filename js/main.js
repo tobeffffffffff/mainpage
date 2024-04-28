cf331a7a99f4644399ed49b8a9325604240aec32
shuiguo <shuiguo@qunhemail.com>
2024-04-28 09:10:13 +0800

    haihaihai

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 import AOS from 'aos';
 import fullpage from 'fullpage.js';
+import Swiper from 'swiper';
+
 import 'aos/dist/aos.css';
 import 'fullpage.js/dist/fullpage.css';
+import '../node_modules/swiper/swiper.css';
+
 import $ from 'jquery';
 
+// 初始化aos
 AOS.init();
 
+// 创建fullpage实例
 var elements = document.querySelectorAll('[data-aos]');
 elements.forEach(function(element) {
   element.classList.add("aos-init");
 });
-
 var fullPageInstance = new fullpage('#fullpage', {
     navigation: true,
     verticalCentered: false,
     anchors:['page1', 'page2', 'page3', 'page4'],
+    menu: "#myMenu",
     onLeave: function(){
         $('.section [data-aos]').each(function(){
             $(this).removeClass("aos-animate")
@@ -37,3 +43,7 @@ var fullPageInstance = new fullpage('#fullpage', {
     }
 });
 
+// 创建swiper
+const swiper = new Swiper('.swiper-container', {
+
+});
